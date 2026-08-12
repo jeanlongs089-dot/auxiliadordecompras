a66cec401c2dd5619de93424eef9b9efce4e9a00
jean <jeanlongs089@gmail.com>
2026-08-12 11:50:59 -0300

    fix: use JavaScript-compatible Sites worker

--- sites-worker.ts
+++ sites-worker.ts
@@ -1,5 +1,5 @@
 export default {
-  async fetch(request: Request): Promise<Response> {
+  async fetch(request) {
     const url = new URL(request.url)
     const response = await fetch(new URL(url.pathname, url.origin), request)
     if (response.status !== 404 || url.pathname.includes('.')) return response
